379197b5274edc9b37fbfdcc8c79c9ea6761de58
JenniBeadle <jenni_beadle@sil.org>
2022-07-23 22:19:50 +0800

    more configuration

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -139,35 +139,3 @@ const config = {
 };
 
 module.exports = config;
-
-plugins: [
-    [
-      'docusaurus-plugin-papersaurus',
-      {
-        keepDebugHtmls: false,
-        sidebarNames: ['someSidebar'],
-        rootDocIds: [
-          { version: 'default', rootDocId: 'start_overview'}
-        ],
-        addDownloadButton: true,
-        autoBuildPdfs: true,
-        downloadButtonText: 'Download as PDF',
-        ignoreDocs: ['licenses'],
-        stylesheets: [],
-        scripts: [],
-        coverPageHeader: `...`,
-        coverPageFooter: `...`,
-        getPdfCoverPage: (siteConfig, pluginConfig, pageTitle, version) => {
-          return `...`;
-        },
-        getPdfPageHeader: (siteConfig, pluginConfig, pageTitle) => {
-          return `...`;
-        },
-        getPdfPageFooter: (siteConfig, pluginConfig, pageTitle) => {
-          return `...`;
-        },
-        author: 'Author name',
-        footerParser: /© SIL International. \d{4}-\d{2}-\d{2}Page \d* \/ \d*/g,
-      },
-    ],
-  ]
